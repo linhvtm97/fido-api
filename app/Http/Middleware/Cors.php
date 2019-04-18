817d3566ffb143572d7a867e5ede78aadc9c9f48
linh <linhvtm@tech.est-rouge.com>
2019-04-18 22:27:08 +0700

    feat(Cors): enable cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,11 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header(‘Access-Control-Allow-Origin’, ‘*’)
-        ->header(‘Access-Control-Allow-Methods’, ‘GET, POST, PUT, DELETE, OPTIONS’)
-        ->header(‘Access-Control-Allow-Headers’, ‘X-Requested-With, Content-Type, X-Token-Auth, Authorization’);
+            ->header('Access-Control-Allow-Origin', $_SERVER['HTTP_ORIGIN'])
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'POST, OPTIONS')
+            ->header('Access-Control-Allow-Credentials', 'true')
+            ->header('Access-Control-Max-Age', '10000')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
     }
 }
