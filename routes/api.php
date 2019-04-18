09a8cbf9bfe6056df5f1699148e697cf2ac8a148
linh <linhvtm@tech.est-rouge.com>
2019-04-18 23:22:18 +0700

    feat(Cors): test cors

--- routes/api.php
+++ routes/api.php
@@ -19,42 +19,44 @@ Route::group(['middleware' => ['cors', 'api']], function () {
     Route::post('/signup', 'UserController@signup');
 
     Route::post('/signout', 'UserController@signout');
+
+    Route::prefix('groups')->group(function () {
+        Route::get('/', 'GroupController@index');
+    
+        Route::get('/{id}', 'GroupController@show');
+    
+        Route::post('/', 'GroupController@store');
+    
+        Route::put('/{id}', 'GroupController@update');
+    
+        Route::delete('/{id}', 'GroupController@destroy');
+    });
+    
+    Route::prefix('users')->group(function () {
+        Route::get('/', 'UserController@index');
+    
+        Route::get('/{id}', 'UserController@show');
+    
+        Route::post('/', 'UserController@store');
+    
+        Route::put('/{id}', 'UserController@update');
+    
+        Route::delete('/{id}', 'UserController@destroy');
+    });
+    
+    Route::prefix('doctors')->group(function () {
+        Route::get('/', 'DoctorController@index');
+    
+        Route::get('/{id}', 'DoctorController@show');
+    
+        Route::post('/', 'DoctorController@store');
+    
+        Route::put('/{id}', 'DoctorController@update');
+    
+        Route::delete('/{id}', 'DoctorController@destroy');
+    });
+    
 });
 
-Route::prefix('groups')->group(function () {
-    Route::get('/', 'GroupController@index');
-
-    Route::get('/{id}', 'GroupController@show');
-
-    Route::post('/', 'GroupController@store');
-
-    Route::put('/{id}', 'GroupController@update');
-
-    Route::delete('/{id}', 'GroupController@destroy');
-});
-
-Route::prefix('users')->group(function () {
-    Route::get('/', 'UserController@index');
-
-    Route::get('/{id}', 'UserController@show');
-
-    Route::post('/', 'UserController@store');
-
-    Route::put('/{id}', 'UserController@update');
-
-    Route::delete('/{id}', 'UserController@destroy');
-});
-
-Route::prefix('doctors')->group(function () {
-    Route::get('/', 'DoctorController@index');
-
-    Route::get('/{id}', 'DoctorController@show');
-
-    Route::post('/', 'DoctorController@store');
-
-    Route::put('/{id}', 'DoctorController@update');
-
-    Route::delete('/{id}', 'DoctorController@destroy');
-});
 
 Auth::routes();
