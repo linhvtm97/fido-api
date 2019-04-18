8c8b3dad0bfe00ac4bf7524a2e9ee87e1aa007c8
linh <linhvtm@tech.est-rouge.com>
2019-04-18 23:19:36 +0700

    feat(Cors): enable cors

--- public/index.php
+++ public/index.php
@@ -48,9 +48,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
