5fc9d07d36a91ca11f920053a5844d20b6f1ae63
linh <linhvtm@tech.est-rouge.com>
2019-05-04 11:13:39 +0700

    fix(doctor): fix store doctor

--- app/Doctor.php
+++ app/Doctor.php
@@ -9,7 +9,8 @@ class Doctor extends Model
     protected $fillable = [
         'name', 'email', 'phone_number', 'status', 'id_number', 'id_number_place', 'id_number_date', 'gender',
         'birthday', 'avatar', 'address_id', 'fk_employee_id', 'created_by_user', 'doctor_no', 'specialist_id',
-        'sub_specialist_id','hospital_name', 'passport_no', 'passport_place', 'passport_date'
+        'sub_specialist_id','hospital_name', 'passport_no', 'passport_place', 'passport_date', 'description',
+        'experience', 'address_details'
     ];
 
     protected $hidden = [
