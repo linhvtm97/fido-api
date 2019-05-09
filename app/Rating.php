993d9a154dd3664d9235c677f8f373ee042585e2
linh <linhvtm@tech.est-rouge.com>
2019-05-09 23:27:20 +0700

    fix(question): fix patient, doctor with rating question api

--- app/Rating.php
+++ app/Rating.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     protected $fillable = ['star', 'like', 'report','patient_id', 'doctor_id', 'review','patient_avatar', 'patient_name'];
+
+    public function doctor()
+    {
+        return $this->belongsTo('App\Doctor', 'doctor_id');
+    }
+
+    public function patient()
+    {
+        return $this->belongsTo('App\Patient', 'patient_id');
+    }
 }
