901a81abc3d856ee3e508a900bf0a2c06f587e23
linh <linhvtm@tech.est-rouge.com>
2019-06-06 17:45:04 +0700

    fix conflict

--- config/database.php
+++ config/database.php
@@ -39,6 +39,13 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
+        
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'prefix' => '',
+            'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
+        ],
 
         'mysql' => [
             'driver' => 'mysql',
